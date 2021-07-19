661ab4978f6ebc21f194018a33ab62c5b69cd776
Khouw Devin Nathanael <khouwdevin@gmail.com>
2021-07-19 09:24:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/no1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.no1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button clickbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        clickbutton = findViewById(R.id.clickbutton);
+
+        clickbutton.setOnClickListener(clickListener);
+    }
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+        }
+    };
+}
